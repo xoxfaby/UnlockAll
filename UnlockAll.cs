b6ff1d46c3f61f55b279e8f22ceec47a42bef560
xoxfaby <xoxfaby@gmail.com>
2022-03-02 22:27:55 +0100

    remove StatSheet_New hook

--- UnlockAll.cs
+++ UnlockAll.cs
@@ -4,7 +4,7 @@ using BepInEx;
 
 namespace UnlockAll
 {
-    [BepInPlugin("com.xoxfaby.UnlockAll", "UnlockAll", "1.0.1")]
+    [BepInPlugin("com.xoxfaby.UnlockAll", "UnlockAll", "1.0.3")]
     public class UnlockAllPlugin : BetterUnityPlugin.BetterUnityPlugin<UnlockAllPlugin>
     {
         public override BaseUnityPlugin typeReference => throw new NotImplementedException();
@@ -13,13 +13,6 @@ namespace UnlockAll
         {
             base.Awake();
             UnlockAllPlugin.Hooks.Add<System.Xml.Linq.XElement, string, RoR2.Stats.StatSheet>(typeof(RoR2.XmlUtility), "GetStatsField", XmlUtility_GetStatsField);
-            UnlockAllPlugin.Hooks.Add<RoR2.Stats.StatSheet>(typeof(RoR2.Stats.StatSheet), "New", StatSheet_New);
-        }
-        private static RoR2.Stats.StatSheet StatSheet_New(Func<RoR2.Stats.StatSheet> orig)
-        {
-            var statSheet = orig();
-            Array.Copy(UnlockableCatalog.indexToDefTable, statSheet.unlockables, UnlockableCatalog.indexToDefTable.Length);
-            return statSheet;
         }
 
         private static void XmlUtility_GetStatsField(Action<System.Xml.Linq.XElement, string, RoR2.Stats.StatSheet> orig, System.Xml.Linq.XElement container, string fieldName, RoR2.Stats.StatSheet dest)
