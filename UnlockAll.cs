b496df573eb1e3a2212de28ef07da37e376c7519
xoxfaby <xoxfaby@gmail.com>
2022-03-02 22:09:20 +0100

    Added hook for new profiles

--- UnlockAll.cs
+++ UnlockAll.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using RoR2;
-using RoR2.Skills;
 using BepInEx;
-using UnityEngine;
-
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
-using UnityEngine.SceneManagement;
 
 namespace UnlockAll
 {
-    [BepInPlugin("com.xoxfaby.UnlockAll", "UnlockAll", "1.0.0")]
+    [BepInPlugin("com.xoxfaby.UnlockAll", "UnlockAll", "1.0.1")]
     public class UnlockAllPlugin : BetterUnityPlugin.BetterUnityPlugin<UnlockAllPlugin>
     {
         public override BaseUnityPlugin typeReference => throw new NotImplementedException();
 
         protected override void Awake()
         {
+            base.Awake();
             UnlockAllPlugin.Hooks.Add<System.Xml.Linq.XElement, string, RoR2.Stats.StatSheet>(typeof(RoR2.XmlUtility), "GetStatsField", XmlUtility_GetStatsField);
+            UnlockAllPlugin.Hooks.Add<RoR2.Stats.StatSheet>(typeof(RoR2.Stats.StatSheet), "New", StatSheet_New);
+        }
+        private static RoR2.Stats.StatSheet StatSheet_New(Func<RoR2.Stats.StatSheet> orig)
+        {
+            var statSheet = orig();
+            Array.Copy(UnlockableCatalog.indexToDefTable, statSheet.unlockables, UnlockableCatalog.indexToDefTable.Length);
+            return statSheet;
         }
 
         private static void XmlUtility_GetStatsField(Action<System.Xml.Linq.XElement, string, RoR2.Stats.StatSheet> orig, System.Xml.Linq.XElement container, string fieldName, RoR2.Stats.StatSheet dest)
