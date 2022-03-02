6d4b9fd9afad58806d93e3654764199bd9d782b9
xoxfaby <xoxfaby@gmail.com>
2022-03-02 20:43:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# UnlockAll
+
+WARNING: This will **permanently** unlock everything for any profile you load.
+
+## Support Me
+
+If you like what I'm doing, consider supporting me through GitHub Sponsors so I can keep doing it:
+
+https://github.com/sponsors/xoxfaby (preferred)  
+https://patreon.com/xoxfaby  
+or directly through paypal:  
+https://streamelements.com/xoxfaby/tip  
+
+## Help & Feedback
+
+If you need help or have suggestions, create an issue on github, join my discord or find me on the RoR2 Modding Discord 
+
+[My Discord](https://discord.gg/Zy2HSB4) XoXFaby#1337
+
+https://github.com/xoxfaby/UnlockAll
+
+## Changelog
+
+## v1.0.0
+ - Initial Release
---
+++ UnlockAll.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using RoR2;
+using RoR2.Skills;
+using BepInEx;
+using UnityEngine;
+
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+namespace UnlockAll
+{
+    [BepInPlugin("com.xoxfaby.UnlockAll", "UnlockAll", "1.0.0")]
+    public class UnlockAllPlugin : BetterUnityPlugin.BetterUnityPlugin<UnlockAllPlugin>
+    {
+        public override BaseUnityPlugin typeReference => throw new NotImplementedException();
+
+        protected override void Awake()
+        {
+            UnlockAllPlugin.Hooks.Add(typeof(RoR2.XmlUtility), "GetStatsField", XmlUtility_GetStatsField);
+        }
+
+        private static void XmlUtility_GetStatsField(Action<System.Xml.Linq.XElement, string, RoR2.Stats.StatSheet> orig, System.Xml.Linq.XElement container, string fieldName, RoR2.Stats.StatSheet dest)
+        {
+            orig(container, fieldName, dest);
+            foreach(var unlockableDef in UnlockableCatalog.indexToDefTable)
+            {
+                dest.AddUnlockable(unlockableDef);
+            }
+        }
+
+    }
+}
